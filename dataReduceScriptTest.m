294ebf08edf37526e60248b85aa8c4f5357a5ad0
Anthony Bisulco <abisulco@gmail.com>
2016-12-08 10:59:19 +0100

    Plots for Data

--- dataReduceScriptTest.m
+++ dataReduceScriptTest.m
@@ -6,16 +6,23 @@
 %total data so for example peakNumber 1=sum(Normilization*Pixel(i,j,:))
 %Therefore small ampltiude pixels do not have as strong a presenece in the
 %reconstruction
+load('Data/cs10372.mat')
 sharing=sharingNorma(cutCube.A2,oldCube.A2,cpp.p2);%This function returns the normlization matrix needed for the reconstruction
 dataPeaks=datatoPeaks(cutCube.A2,oldCube.A2,sharing);%Performs mutiplication and summing using this normilization to return data as a function of peaks
 figure%plotting scheme
 for i=1:length(dataPeaks(:,1))
-    plot(dataPeaks(i,:),'linewidth',3);%plots all the invidual peaks
+    if i==1
+    plot(10.*dataPeaks(i,:),'linewidth',3);%plots all the invidual peaks
+
+    else
+       plot(dataPeaks(i,:),'linewidth',3);%plots all the invidual peaks
+   
+    end
     hold all
 end
 plot(oldCube.A2,'linewidth',3)%plots the old PMT
 xlabel('Triggers')%Titles
 ylabel('ADC Counts')
-title('Data Reduction from 64 Channels to only 13 function fo the peak');
+title('372:Data Reduction from 64 Channels to only 13 function fo the peak');
 set(gca,'fontsize',24);
 optFitLeastSquaresPeaks(dataPeaks,oldCube.A2) %Performs a weighted summing scheme to solve |oldCube.A2-alpha*dataPeaks(i,:)|<epsilon and plots the summed scheme
